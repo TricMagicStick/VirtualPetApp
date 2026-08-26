2ff85e2ad8f9e89e1fa6eb658cf87d9aeb30b7f5
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-08-26 16:01:47 -0700

    Add puff/bud/bolt pixel opcode sheets

---
+++ sprites-bud.js
@@ -0,0 +1,10 @@
+if (typeof SPRITE_OPS === 'undefined') { var SPRITE_OPS = {}; }
+if (typeof SPRITE_FACE === 'undefined') { var SPRITE_FACE = {}; }
+SPRITE_OPS.draw_bud = 'E26,52,4,2,d|E36,52,4,2,d|E32,46,7,6,b|E32,47,4,3,belly|E32,32,13,12,b|E28,28,6,5,l|E32,38,6,4,belly|R31,16,2,7,stem|E27,16,5,3,leaf|E37,16,5,3,leaf|O';
+SPRITE_FACE.draw_bud = [26,31,38,31,3,3,32,38];
+SPRITE_OPS.draw_sprout = 'E26,56,5,3,d|E38,56,5,3,d|R25,46,5,10,m|R36,46,5,10,m|E32,42,9,10,b|E32,44,5,6,belly|E18,40,7,4,leaf|E46,40,7,4,leaf|T14,40,10,34,20,38,m|T50,40,54,34,44,38,m|E32,26,11,10,b|E29,23,5,4,l|R31,12,2,8,stem|E26,12,6,4,leaf|E38,11,6,4,leaf|O';
+SPRITE_FACE.draw_sprout = [27,25,37,25,3,2,32,31];
+SPRITE_OPS.draw_sprig = 'E24,58,6,3,d|E40,58,6,3,d|R22,44,6,14,m|R38,44,6,14,m|E24,46,4,4,leaf|E40,46,4,4,leaf|E32,38,11,12,b|E32,40,6,7,belly|E16,34,8,5,leaf|E48,34,8,5,leaf|R14,38,4,12,stem|R48,38,4,12,stem|E14,50,5,3,leaf|E50,50,5,3,leaf|E32,22,10,9,b|E29,19,5,4,l|R31,8,3,8,stem|E24,8,7,5,leaf|E40,7,7,5,leaf|E32,5,5,4,m|O';
+SPRITE_FACE.draw_sprig = [27,21,37,21,3,2,32,27];
+SPRITE_OPS.draw_verdant = 'E22,60,7,3,d|E42,60,7,3,d|R20,44,7,16,m|R40,44,7,16,m|E22,48,6,5,leaf|E42,48,6,5,leaf|E32,36,12,14,b|E32,38,7,8,belly|T20,28,4,44,22,50,leaf|T44,28,60,44,42,50,leaf|E10,42,6,8,m|E54,42,6,8,m|E8,52,5,4,leaf|E56,52,5,4,leaf|E32,24,5,5,b|E32,16,9,8,b|E29,14,4,3,l|E32,2,4,3,pink|E39,5,4,3,pink|E25,5,4,3,pink|E42,9,4,3,pink|E22,9,4,3,pink|D32,8,3,pink2|D32,8,1,gold|E26,6,3,2,pink3|E38,6,3,2,pink3|E22,10,4,3,leaf|E42,10,4,3,leaf|O';
+SPRITE_FACE.draw_verdant = [28,16,36,16,2,2,32,21];
