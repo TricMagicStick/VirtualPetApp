a246c3a2a033f0d55e6919dcf5879bf35a449ec4
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-09-01 22:50:04 -0700

    Add Spark locked sprite loader (3/4 biped override).

---
+++ sprites-pico-spark-locked.js
@@ -0,0 +1,3 @@
+window.__PICO_SPRITES = window.__PICO_SPRITES || {};
+window.__PICO_SPRITES['spark-happy'] = (window.__PICO_PARTS_spark_happy || []).join('');
+window.__PICO_SPRITES['spark-sad'] = (window.__PICO_PARTS_spark_sad || []).join('');
