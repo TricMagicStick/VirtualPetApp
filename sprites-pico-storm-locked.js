0d6ac501fb23a4f1b649b267adf3619e9d34f07d
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-09-01 17:04:24 -0700

    Swap live Storm to Tyson locked winged adult

--- sprites-pico-storm-locked.js
+++ sprites-pico-storm-locked.js
@@ -1,1 +1,7 @@
-/workspace/play-cdn/sprites-pico-storm-locked.js
+window.__PICO_SPRITES = window.__PICO_SPRITES || {};
+(function(){
+  function load(u){var x=new XMLHttpRequest();x.open('GET',u,false);x.send();return x.responseText;}
+  var s='';
+  for(var i=0;i<7;i++) s+=load('sprites-pico-storm-locked-p'+i+'.js');
+  eval(s);
+})();
