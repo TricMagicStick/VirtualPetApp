7d6cd576ab625679cae7f2b190dd35c0b5411587
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-09-02 23:59:02 -0700

    Ship locked 48px Whisp/Whisk/Nimbrix happy sprites

---
+++ sprites-pico-air-locked.js
@@ -0,0 +1,7 @@
+window.__PICO_SPRITES = window.__PICO_SPRITES || {};
+window.__PICO_SPRITES['whisp-happy'] = (window.__PICO_PARTS_whisp_happy || []).join('');
+window.__PICO_SPRITES['whisp-sad'] = window.__PICO_SPRITES['whisp-happy'];
+window.__PICO_SPRITES['whisk-happy'] = (window.__PICO_PARTS_whisk_happy || []).join('');
+window.__PICO_SPRITES['whisk-sad'] = window.__PICO_SPRITES['whisk-happy'];
+window.__PICO_SPRITES['nimbrix-happy'] = (window.__PICO_PARTS_nimbrix_happy || []).join('');
+window.__PICO_SPRITES['nimbrix-sad'] = window.__PICO_SPRITES['nimbrix-happy'];
