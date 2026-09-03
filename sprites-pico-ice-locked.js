8bd379720724562a80aed35de5a84e1348ba3478
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-09-02 17:16:39 -0700

    Add ice-locked sprite assembler (sad keys alias happy).

---
+++ sprites-pico-ice-locked.js
@@ -0,0 +1,7 @@
+window.__PICO_SPRITES = window.__PICO_SPRITES || {};
+window.__PICO_SPRITES['rime-happy'] = (window.__PICO_PARTS_rime_happy || []).join('');
+window.__PICO_SPRITES['rime-sad'] = window.__PICO_SPRITES['rime-happy'];
+window.__PICO_SPRITES['kryz-happy'] = (window.__PICO_PARTS_kryz_happy || []).join('');
+window.__PICO_SPRITES['kryz-sad'] = window.__PICO_SPRITES['kryz-happy'];
+window.__PICO_SPRITES['glacorn-happy'] = (window.__PICO_PARTS_glacorn_happy || []).join('');
+window.__PICO_SPRITES['glacorn-sad'] = window.__PICO_SPRITES['glacorn-happy'];
